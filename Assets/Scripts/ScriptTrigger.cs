1d74202acb7abf5a8d3e33167b75d8750197b254
Eric Warrington <esw5886@rit.edu>
2016-11-12 19:18:22 -0500

    Initial commit for ScriptTriggers

---
+++ Assets/Scripts/ScriptTrigger.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+
+    public string sceneName;
+
+    private bool inTrigger = false; 
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(sceneName);
+        if (inTrigger && sceneName != "")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                SceneManager.LoadScene(sceneName);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        inTrigger = true;
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        inTrigger = false;
+    }
+}
