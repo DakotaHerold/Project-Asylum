37bda5139942dd6d0f1c162fba0738f183b6ffb4
Eric Warrington <esw5886@rit.edu>
2016-11-13 22:03:49 -0500

    Finished prototyping camera-target change ScriptTrigger

---
+++ Assets/Scripts/YATestGameScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class YATestGameScript: GameScript
+{
+	public SmoothFollow2D smoothCam;
+	public CharacterController player;
+	public CharacterController npc;
+
+	//private bool hasRun=false;
+
+
+	//called by the ScriptTrigger
+	public override void Run()
+	{
+		if(didPlay)
+			return;
+
+		print("Start!");
+
+		StartCoroutine(RunScript());
+	}
+
+	private IEnumerator RunScript()
+	{
+		if(smoothCam!=null)
+		{
+			player.canMove=false;	//lock
+			smoothCam.setTarget(npc.transform);
+			npc.Move(5f, 0f);
+
+			//wait some seconds
+			yield return new WaitForSeconds(3);
+
+
+			smoothCam.setTarget(player.transform);
+			player.canMove=true;	//unlock
+
+			//do more stuff...
+			//...
+
+			didPlay=true;
+		}
+	}
+}
