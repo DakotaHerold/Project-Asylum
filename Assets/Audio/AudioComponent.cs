8150c369b2612709d716f68c619868190fc34a83
Forrest argzero S <fzs7217@rit.edu>
2016-11-14 00:33:49 -0500

    AZ Audiomanager and 4 audio tracks

---
+++ Assets/Audio/AudioComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioComponent : MonoBehaviour {
+	public AudioSource source;
+	public float volume;
+	public const float CHANGE = 0.05f;
+	void Start(){
+		source = gameObject.GetComponent<AudioSource>();
+	}
+
+	void Update(){
+		if(source.volume!=volume){
+            if (Mathf.Abs(source.volume - volume) < CHANGE) {
+                source.volume = volume;
+                return;
+            }
+			source.volume = Mathf.Lerp(source.volume, volume, CHANGE);
+		}
+	}
+}
---
+++ Assets/Audio/AudioManager.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AudioManager : MonoBehaviour {
+    public AudioManager Instance;
+	public List<AudioComponent> components;
+    public bool debugEnabled; 
+    public enum DepressionState
+    {
+        Depressed,
+        DepressedEvent,
+        Cheerful,
+        CheerfulEvent
+    }
+
+    public DepressionState State;
+    
+    // Use this for initialization
+    void Start () {
+        State = DepressionState.Depressed;
+        Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (debugEnabled) {
+            if (Input.GetKey(KeyCode.A))
+            {
+                State = DepressionState.Depressed;
+            }
+            else if (Input.GetKey(KeyCode.B))
+            {
+                State = DepressionState.DepressedEvent;
+            }
+            else if (Input.GetKey(KeyCode.C))
+            {
+                State = DepressionState.Cheerful;
+            }
+            else if (Input.GetKey(KeyCode.D))
+            {
+                State = DepressionState.CheerfulEvent;
+            }
+        }
+        setLevels();
+	}
+
+	// Resets all audio tracks for restarting of the game
+	public void ResetGame(){
+		foreach(AudioComponent a in components){
+			a.source.Stop();
+            State = DepressionState.Depressed;
+			setLevels();
+		}
+	}
+
+	// Starts all audio tracks
+	public void StartGame(){
+		foreach(AudioComponent a in components){
+			a.source.Play();
+		}
+        State = DepressionState.Depressed;
+        setLevels();
+	}
+
+	private void setLevels(){
+        switch (State)
+        {
+            case DepressionState.Depressed:
+                if (components[0].volume < 1.0f)
+                    components[0].volume += 0.1f;
+                if (components[1].volume > 0.0f)
+                    components[1].volume -= 0.1f;
+                if (components[2].volume > 0.0f)
+                    components[2].volume -= 0.1f;
+                if (components[3].volume > 0.0f)
+                    components[3].volume -= 0.1f;
+                break;
+            case DepressionState.DepressedEvent:
+                if (components[0].volume > 0.0f)
+                    components[0].volume -= 0.1f;
+                if (components[1].volume < 1.0f)
+                    components[1].volume += 0.1f;
+                if (components[2].volume > 0.0f)
+                    components[2].volume -= 0.1f;
+                if (components[3].volume > 0.0f)
+                    components[3].volume -= 0.1f;
+                break;
+            case DepressionState.Cheerful:
+                if (components[0].volume > 0.0f)
+                    components[0].volume -= 0.1f;
+                if (components[1].volume > 0.0f)
+                    components[1].volume -= 0.1f;
+                if (components[2].volume < 1.0f)
+                    components[2].volume += 0.1f;
+                if (components[3].volume > 0.0f)
+                    components[3].volume -= 0.1f;
+                break;
+            case DepressionState.CheerfulEvent:
+                if (components[0].volume > 0.0f)
+                    components[0].volume -= 0.1f;
+                if (components[1].volume > 0.0f)
+                    components[1].volume -= 0.1f;
+                if (components[2].volume > 0.0f)
+                    components[2].volume -= 0.1f;
+                if (components[3].volume < 1.0f)
+                    components[3].volume += 0.1f;
+                break;
+        }
+    }
+
+	// Sets the component volume for a track of the song currently being played
+	private void setComponentVolume(AudioComponent a, float volume){
+		a.volume = volume;
+	}
+}
